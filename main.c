abf0744cf322c580ddd15443c02d88057cdbab70
teayoon <yuyu980701@naver.com>
2022-03-02 20:29:15 +0900

    first

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test
+//
+//  Created by 김태윤 on 2022/03/02.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
